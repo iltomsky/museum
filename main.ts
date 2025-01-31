cfe86ff73bf5091358ec865c4264d40d297c84c4
iltomsky <nathan.arnouts2012@gmail.com>
2025-01-31 16:05:26 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(getal)
+    getal += -1
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(getal)
+    getal += 1
+})
+let getal = 0
+getal = 0
